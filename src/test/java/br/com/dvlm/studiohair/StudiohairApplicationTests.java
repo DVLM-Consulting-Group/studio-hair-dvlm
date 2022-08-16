41103871c9a3555d1b2cd7343db864b1311a5544
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-16 12:41:36 -0300

    iniciando projeto

---
+++ src/test/java/br/com/dvlm/studiohair/StudiohairApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dvlm.studiohair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiohairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
