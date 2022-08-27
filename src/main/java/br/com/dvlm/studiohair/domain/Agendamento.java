76b9eb50cc98c0c6c431a96968cb9ac95388448c
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-27 16:20:37 -0300

    criando perfil dev no mysq

--- src/main/java/br/com/dvlm/studiohair/domain/Agendamento.java
+++ src/main/java/br/com/dvlm/studiohair/domain/Agendamento.java
@@ -49,7 +49,7 @@ public class Agendamento {
         this.id = id;
         this.valor = valor;
         this.setDataAgendamento(LocalDateTime.now());
-        this.servico = (servico == null) ? 0 : servico.getCod();
+        this.servico = (servico == null) ? 5 : servico.getCod();
         this.observacoes = observacoes;
         this.status = (status == null) ? 0 : status.getCod();
         this.funcionario = funcionario;
