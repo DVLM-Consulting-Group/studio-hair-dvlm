c2935e10891f9b2ac214c708754b02800c937920
Luan Braz <luan_silvacpv@hotmail.com>
2022-09-15 01:22:22 -0300

    update

--- src/main/java/com/dvlm/studiohair/repositories/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.dvlm.studiohair.repositories;
-
-import br.com.dvlm.studiohair.domain.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
